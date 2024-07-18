efd2edf61c7b0be0be60e0ab88e318c92af04074
jvvppereira <joao.paes@philips.com>
2024-07-17 21:44:56 -0300

    created main structure of trip details page

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { InviteGuestsModal } from "./invite-guests-modal";
 import { ConfirmTripModal } from "./confirm-trip-modal";
 import { DestinationAndDateStep } from "./steps/destination-and-date-step";
+import { InviteGuestsStep } from "./steps/invite-guests-step";
 
 export function CreateTripPage() {
   const navigate = useNavigate()
@@ -83,23 +84,11 @@ export function CreateTripPage() {
           />
 
           {isGuestsInputOpen &&
-            <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-              <button onClick={openGuestsModal} className="flex items-center gap-2 flex-1 text-left">
-                <UserRoundPlus className="size-5 text-zinc-400" />
-                {emailsToInvite.length > 0 ?
-                  <span className="text-zinc-100 text-lg flex-1">{emailsToInvite.length} pessoa(s) convidada(s)</span>
-                  :
-                  <span className="text-zinc-400 text-lg flex-1">Quem estará na viagem?</span>
-                }
-              </button>
-
-              <div className="w-px h-6 bg-zinc-800" />
-
-              <button onClick={openConfirmModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
-                Confirmar viagem
-                <ArrowRight className="size-5" />
-              </button>
-            </div>
+            <InviteGuestsStep
+              emailsToInvite={emailsToInvite}
+              openConfirmModal={openConfirmModal}
+              openGuestsModal={openGuestsModal}
+            />
           }
         </div>
 
--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,5 +1,132 @@
+import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog } from "lucide-react";
 import React from "react";
 
 export function TripDetailsPage() {
-    return (<h1>oi</h1>)
+    return (
+        <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
+            <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                    <MapPin className="size-5 text-zinc-400" />
+                    <span className=" text-zinc-100">Roma, Italy</span>
+                </div>
+
+                <div className="flex items-center gap-5">
+                    <div className=" flex items-center gap-2">
+                        <Calendar className="size-5 text-zinc-400" />
+                        <span className=" text-zinc-100">15 a 20 de Novembro</span>
+                    </div>
+                </div>
+
+                <div className="w-px h-6 bg-zinc-800" />
+
+                <button className="bg-zinc-800 text-zin20 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+                    Alterar local/data
+                    <Settings2 className="size-5" />
+                </button>
+            </div>
+
+            <main className="flex gap-16 px-4">
+                <div className="flex-1 space-y-6">
+                    <div className="flex items-center justify-between">
+                        <h2 className="text-3xl font-semibold">Atividades</h2>
+                        <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                            <Plus className="size-5" />
+                            Cadastrar atividade
+                        </button>
+                    </div>
+
+                    <div className="space-y-8">
+                        <div className="space-y-2.5">
+                            <div className="flex gap-2 items-baseline">
+                                <span className="text-xl text-zinc-300 font-semibold">Dia 15</span>
+                                <span className="text-xs text-zinc-500">Sábado</span>
+                            </div>
+                            <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data.</p>
+                        </div>
+
+                        <div className="space-y-2.5">
+                            <div className="flex gap-2 items-baseline">
+                                <span className="text-xl text-zinc-300 font-semibold">Dia 16</span>
+                                <span className="text-xs text-zinc-500">Domingo</span>
+                            </div>
+
+                            <div className="space-y-2.5">
+                                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                                    <CircleCheck className="size-5 text-lime-300" />
+                                    <span className="text-zinc-100">Kart com a galera</span>
+                                    <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+                                </div>
+                            </div>
+                            <div className="space-y-2.5">
+                                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                                    <CircleCheck className="size-5 text-lime-300" />
+                                    <span className="text-zinc-100">Kart com a galera</span>
+                                    <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-80 space-y-6">
+                    <div className="space-y-6">
+                        <h2 className="font-semibold text-xl">Links importantes</h2>
+                        <div className="space-y-5">
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+                                    <a href="#" className="block truncate text-xs text-zinc-400 hover:text-zinc-200">
+                                        https://www.airbnb.com.br/rooms/1234565345345345234234234234234234
+                                    </a>
+                                </div>
+                                <Link2 className="text-zinc-400 size-5 shrink-0" />
+                            </div>
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+                                    <a href="#" className="block truncate text-xs text-zinc-400 hover:text-zinc-200">
+                                        https://www.airbnb.com.br/rooms/1234565345345345234234234234234234
+                                    </a>
+                                </div>
+                                <Link2 className="text-zinc-400 size-5 shrink-0" />
+                            </div>
+                        </div>
+                        <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
+                            <Plus className="size-5"/>
+                            Cadastrar novo link
+                        </button>        
+                    </div>
+
+                    <div className="w-full h-px bg-zinc-800"></div>
+
+                    <div className="space-y-6">
+                        <h2 className="font-semibold text-xl">Convidados</h2>
+                        <div className="space-y-5">
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Joao Feijao</span>
+                                    <span className="block truncate text-xm text-zinc-400 ">
+                                        joao@feijao.com
+                                    </span>
+                                </div>
+                                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+                            </div>
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                <span className="block font-medium text-zinc-100">Joao Feijao</span>
+                                    <span className="block truncate text-xm text-zinc-400 ">
+                                        joao@feijao.com
+                                    </span>
+                                </div>
+                                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+                            </div>
+                        </div>
+                        <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
+                            <UserCog className="size-5"/>
+                            Gerenciar convidados
+                        </button> 
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
 }
